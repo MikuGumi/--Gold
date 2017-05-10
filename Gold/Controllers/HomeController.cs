5c82677a9213a365d5ee0a056822a7d3ccff7d43
gumi <hska0606@gmail.com>
2017-05-10 18:25:17 +0800

    加入專案檔案。

---
+++ Gold/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Gold.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gold.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Random rand = new Random();
+            var model = new List<GoldViewModels>();
+            GoldViewModels coins;
+
+            for (int i = 0; i < 200; i++)
+            {
+                coins = new GoldViewModels();
+                int type = rand.Next(2);
+                if (type == 0)
+                {
+                    coins.Sort = "支出";
+                }
+                else
+                {
+                    coins.Sort = "收入";
+                }
+                coins.Date = DateTime.Now.AddDays(rand.Next(10000));
+                coins.Coin = rand.Next(10000);
+                model.Add(coins);
+            };
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Gold/Models/GoldViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gold.Models
+{
+    public class GoldViewModels
+    {
+        public string Sort { get; set; }
+        public DateTime Date { get; set; }
+        public int Coin { get; set; }
+    }
+}
